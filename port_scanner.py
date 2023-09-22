91fa3f1377f2e7facbea4162a379c9f5934c459b
steventejeda <steventejeda@gmail.com>
2023-09-21 21:49:27 -0400

    optimized

--- port_scanner.py
+++ port_scanner.py
@@ -1,25 +1,29 @@
 import socket
+import termcolor
+
 
 
 def scan(target, ports):
-    for port in range(1, ports):
-        scan_port(targets, port)
+	print('\n' + ' Starting Scan For ' + str(target))
+	for port in range(1,ports):
+		scan_port(target,port)
 
 
 def scan_port(ipaddress, port):
-    try:
-        sock = socket.socket()
-        sock.connect((ipaddress, port))
-        print("[+] Port Opened" + str(port))
-        sock.close()
-    except:
-        pass 
+	try:
+		sock = socket.socket()
+		sock.connect((ipaddress, port))
+		print("[+] Port Opened " + str(port))
+		sock.close()
+	except:
+		pass
 
-targets = input("[*] Enter Targets to Scan(split them by ,): ")
-ports = int(input("[*] Enter how many ports you want to scan: "))
+
+targets = input("[*] Enter Targets To Scan(split them by ,): ")
+ports = int(input("[*] Enter How Many Ports You Want To Scan: "))
 if ',' in targets:
-    print("[*] Scanning multiple targets")
-    for ip_address in targets.split(','):
-        scan(ip_address.strip(' '), ports)
+	print(termcolor.colored(("[*] Scanning Multiple Targets"), 'green'))
+	for ip_addr in targets.split(','):
+		scan(ip_addr.strip(' '), ports)
 else:
-    scan(targets, ports)
+	scan(targets,ports)
